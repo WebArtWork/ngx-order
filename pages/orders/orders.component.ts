30b1b9d8f5d72b21acdc3293798534b520d0f78c
Narolskyi Denis <asotizianov@gmail.com>
2024-05-16 14:27:37 +0300

    initialize

---
+++ pages/orders/orders.component.ts
@@ -0,0 +1,162 @@
+import { Component } from '@angular/core';
+import { FormService } from 'src/app/modules/form/form.service';
+import { OrderService, Order } from '../../services/order.service';
+import {
+	AlertService,
+	CoreService,
+	MongoService,
+	StoreService as _StoreService
+} from 'wacom';
+import { TranslateService } from 'src/app/modules/translate/translate.service';
+import { FormInterface } from 'src/app/modules/form/interfaces/form.interface';
+import { StoreService, Store } from 'src/app/modules/store/services/store.service';
+
+@Component({
+	templateUrl: './orders.component.html',
+	styleUrls: ['./orders.component.scss']
+})
+export class OrdersComponent {
+	columns = ['name', 'description'];
+
+	form: FormInterface = this._form.getForm('orders', {
+		formId: 'orders',
+		title: 'Orders',
+		components: [
+			{
+				name: 'Text',
+				key: 'name',
+				focused: true,
+				fields: [
+					{
+						name: 'Placeholder',
+						value: 'fill orders title'
+					},
+					{
+						name: 'Label',
+						value: 'Title'
+					}
+				]
+			},
+			{
+				name: 'Text',
+				key: 'description',
+				fields: [
+					{
+						name: 'Placeholder',
+						value: 'fill orders description'
+					},
+					{
+						name: 'Label',
+						value: 'Description'
+					}
+				]
+			},
+			{
+				name: 'Select',
+				key: 'stores',
+				fields: [
+					{
+						name: 'Placeholder',
+						value: 'fill your stores'
+					},
+					{
+						name: 'Label',
+						value: 'Stores'
+					},
+					{
+						name: 'Multiple',
+						value: true
+					},
+					{
+						name: 'Items',
+						value: this.stores
+					}
+				]
+			}
+		]
+	});
+
+	config = {
+		create: () => {
+			this._form.modal<Order>(this.form, {
+				label: 'Create',
+				click: (created: unknown, close: () => void) => {
+					this._os.create(
+						created as Order,
+						this.setOrders.bind(this)
+					);
+					close();
+				}
+			}, this.store ? { stores: [this.store] } : {});
+		},
+		update: (doc: Order) => {
+			this._form
+				.modal<Order>(this.form, [], doc)
+				.then((updated: Order) => {
+					this._core.copy(updated, doc);
+					this._os.save(doc);
+				});
+		},
+		delete: (doc: Order) => {
+			this._alert.question({
+				text: this._translate.translate(
+					'Common.Are you sure you want to delete this cservice?'
+				),
+				buttons: [
+					{
+						text: this._translate.translate('Common.No')
+					},
+					{
+						text: this._translate.translate('Common.Yes'),
+						callback: () => {
+							this._os.delete(doc);
+						}
+					}
+				]
+			});
+		}
+	};
+
+	orders: Order[] = [];
+	setOrders() {
+		this.orders.splice(0, this.orders.length);
+		for (const order of this._os.orders) {
+			order.stores = order.stores || [];
+			if (this.store) {
+				if (order.stores.includes(this.store)) {
+					this.orders.push(order);
+				}
+			} else {
+				this.orders.push(order);
+			}
+		}
+	}
+
+	update(order: Order) {
+		this._os.update(order);
+	}
+
+	get stores(): Store[] {
+		return this._ss.stores;
+	}
+	store: string;
+	setStore(store: string) {
+		this.store = store;
+		this._store.set('store', store);
+		this.setOrders();
+	}
+
+	constructor(
+		private _os: OrderService,
+		private _translate: TranslateService,
+		private _alert: AlertService,
+		private _form: FormService,
+		private _core: CoreService,
+		private _store: _StoreService,
+		private _ss: StoreService,
+		private _mongo: MongoService
+	) {
+		this._store.get('store', this.setStore.bind(this));
+		this._mongo.on('order', this.setOrders.bind(this));
+	}
+}
---
+++ services/order.service.ts
@@ -0,0 +1,87 @@
+import { Injectable } from '@angular/core';
+import { MongoService, AlertService } from 'wacom';
+
+export interface Order {
+	_id: string;
+	name: string;
+	description: string;
+	stores: string[];
+}
+
+@Injectable({
+	providedIn: 'root'
+})
+export class OrderService {
+	orders: Order[] = [];
+
+	_orders: any = {};
+
+	new(): Order {
+		return {} as Order;
+	}
+
+	constructor(
+		private mongo: MongoService,
+		private alert: AlertService
+	) {
+		this.orders = mongo.get('order', {}, (arr: any, obj: any) => {
+			this._orders = obj;
+		});
+	}
+
+	create(
+		order: Order = this.new(),
+		callback = (created: Order) => {},
+		text = 'order has been created.'
+	) {
+		if (order._id) {
+			this.save(order);
+		} else {
+			this.mongo.create('order', order, (created: Order) => {
+				callback(created);
+				this.alert.show({ text });
+			});
+		}
+	}
+
+	doc(orderId: string): Order {
+		if(!this._orders[orderId]){
+			this._orders[orderId] = this.mongo.fetch('order', {
+				query: {
+					_id: orderId
+				}
+			});
+		}
+		return this._orders[orderId];
+	}
+
+	update(
+		order: Order,
+		callback = (created: Order) => {},
+		text = 'order has been updated.'
+	): void {
+		this.mongo.afterWhile(order, ()=> {
+			this.save(order, callback, text);
+		});
+	}
+
+	save(
+		order: Order,
+		callback = (created: Order) => {},
+		text = 'order has been updated.'
+	): void {
+		this.mongo.update('order', order, () => {
+			if(text) this.alert.show({ text, unique: order });
+		});
+	}
+
+	delete(
+		order: Order,
+		callback = (created: Order) => {},
+		text = 'order has been deleted.'
+	): void {
+		this.mongo.delete('order', order, () => {
+			if(text) this.alert.show({ text });
+		});
+	}
+}
